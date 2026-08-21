baaa7e98d0a4f09a026b9e0c78a1290e2a837126
Jess <itsjessieyt@gmail.com>
2026-08-20 23:07:14 -0400

    Add drag-and-drop functionality to start button

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ setInterval(() => {
         video.play();
     }
 }, 100);
+
+const button = document.getElementById("startButton");
+
+let dragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+button.addEventListener("mousedown", (event) => {
+    dragging = true;
+    offsetX = event.clientX - button.offsetLeft;
+    offsetY = event.clientY - button.offsetTop;
+});
+
+document.addEventListener("mousemove", (event) => {
+    if (!dragging) return;
+
+    button.style.left = `${event.clientX - offsetX}px`;
+    button.style.top = `${event.clientY - offsetY}px`;
+});
+
+document.addEventListener("mouseup", () => {
+    dragging = false;
+});
