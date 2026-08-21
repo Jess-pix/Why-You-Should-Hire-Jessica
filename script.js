4e535bd787c736626d9cf9bab97ff50e57aa4487
Jess <itsjessieyt@gmail.com>
2026-08-21 09:22:47 -0400

    Update button positioning logic in script.js

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 const video = document.querySelector("#titleScreen");
 const button = document.getElementById("startButton");
 
-// Your button's position when it looks correct.
-const reference = {
-    left: 1155.9,
-    top: 212.8,
-    width: 341,
-    height: 100
-};
-
-// Your browser size when that position was correct.
-const referenceScreen = {
-    width: 1536,
-    height: 864
+const buttonReference = {
+    left: 75.2539,
+    top: 19.1327,
+    width: 22.2005,
+    height: 14.0818
 };
 
 function positionStartButton() {
-    const scaleX = window.innerWidth / referenceScreen.width;
-    const scaleY = window.innerHeight / referenceScreen.height;
+    const videoRect = video.getBoundingClientRect();
 
-    button.style.left = `${reference.left * scaleX}px`;
-    button.style.top = `${reference.top * scaleY}px`;
-    button.style.width = `${reference.width * scaleX}px`;
-    button.style.height = `${reference.height * scaleY}px`;
+    button.style.left =
+        `${videoRect.left + videoRect.width * buttonReference.left / 100}px`;
+
+    button.style.top =
+        `${videoRect.top + videoRect.height * buttonReference.top / 100}px`;
+
+    button.style.width =
+        `${videoRect.width * buttonReference.width / 100}px`;
+
+    button.style.height =
+        `${videoRect.height * buttonReference.height / 100}px`;
 }
 
 positionStartButton();
-
 window.addEventListener("resize", positionStartButton);
 
 setInterval(() => {
