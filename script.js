745a026c92a00ce92ca818054a58181bcc120601
Jess <itsjessieyt@gmail.com>
2026-08-20 22:29:22 -0400

    Change video event listener to 'timeupdate'

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const video = document.querySelector("video");
 
-video.addEventListener("ended", () => {
-    video.currentTime = 1;
-    video.play();
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 10.9) {
+        video.currentTime = 1;
+        video.play();
+    }
 });
